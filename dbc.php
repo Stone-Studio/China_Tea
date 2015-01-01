7d931169f71badb4495b16192702004ebe1e2cf4
Stone-Studio <Wozl777774@yandex.ru>
2015-01-01 15:02:53 +0400

    Delete dbc.php

--- dbc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $host = '';
-    $user = '';
-    $password = '';
-    $base = '';
-
-    $dbc = mysqli_connect($host, $user, $password, $base)
-    or die ('Ошибка подключения к Базе Данных');
